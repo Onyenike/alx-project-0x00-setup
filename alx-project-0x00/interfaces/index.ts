f2eba7f327eedd0e999ce87d3ff7e586daf20a79
onyenike <onyenikefestus2019@gmail.com>
2025-11-21 04:39:47 -0800

    Update index.ts

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,8 +1,11 @@
 export interface PillProps {
   title: string
 }
+// interfaces/index.tsx
+import { ButtonHTMLAttributes, ReactNode } from "react";
 export interface ButtonProps {
   title: string;
+  "styles: string"
   size: 'small' | 'medium' | 'large';
   shape: 'rounded-sm' | 'rounded-md' | 'rounded-full';
   className?: string;
